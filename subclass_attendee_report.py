512a726f529b47284b3511441242214f0462b871
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-06 11:27:59 +0100

    Pseudo complete

---
+++ subclass_attendee_report.py
@@ -0,0 +1,21 @@
+from class_flights import *
+
+#Pseudo
+#TDD
+#Code
+
+class AttendeeReport(Flight):
+    pass
+
+# Generate dict
+
+# Passenger: name, dob, passport_no, ticket_type(sales), boarding_pass(sales)
+# Crew: name, dob, title
+
+# Name, title, dob, passport_no, ticket_type, boarding_pass     <-----------
+    # title - attendant, pilot, passenger (if crew is not True) <-----------
+
+# Present aesthetically
+# Display everything
+# for key, value in dict.items():
+#    print(key, ": ", value)
