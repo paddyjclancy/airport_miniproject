0031d6089cff9185735815d564d1c5c2e0fe53d2
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-06 10:48:24 +0100

    Created repo / files

---
+++ class_flights.py
@@ -0,0 +1,8 @@
+
+# Parent Class = Flight()
+    #
+# Sub Classes = FlightReport(), AttendeeReport(), Cities(), Planes()
+    # FlightReport() -
+    # AttendeeReport() -
+    # Cities() - destination, distance
+    # Planes() - model, capacity, cruising_speed
---
+++ class_people.py
@@ -0,0 +1,9 @@
+
+# Parent Class = People()
+    # full_name, dob, nationality
+# Sub classes = Passengers, Staff, Sales
+    # Passengers() - passport_no
+    # Staff() - title
+    # Sales() - ticket_type, boarding_pass
+
+## Methods:
