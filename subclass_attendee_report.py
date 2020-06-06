dcb9ae44f554cecddf4e6f3d06f5b2ba2c631594
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-06 17:28:20 +0100

    Build and test it.1

--- subclass_attendee_report.py
+++ subclass_attendee_report.py
@@ -1,21 +1,36 @@
 from class_flights import *
+from class_people import *
+from sub_class_passengers import *
+from subclass_staff import *
 
-#Pseudo
-#TDD
-#Code
 
-class AttendeeReport(Flight):
-    pass
-
-# Generate dict
+# Append list with dict
 
 # Passenger: name, dob, passport_no, ticket_type(sales), boarding_pass(sales)
 # Crew: name, dob, title
 
 # Name, title, dob, passport_no, ticket_type, boarding_pass     <-----------
-    # title - attendant, pilot, passenger (if crew is not True) <-----------
+# title - attendant, pilot, passenger (if crew is not True) <-----------
 
 # Present aesthetically
 # Display everything
 # for key, value in dict.items():
 #    print(key, ": ", value)
+
+
+class AttendeeReport(Flight):
+
+    def __init__(self, flight, manifest=None):
+        self.flight = flight.capitalize()
+        self.manifest = manifest
+        if manifest is None:
+            self.manifest = []
+
+
+    def append_manifest(self, passenger):
+        self.manifest.append(passenger)
+
+
+    def print_manifest(self):
+        for person in self.manifest:
+            print(vars(person))
---
+++ test_attendee_report.py
@@ -0,0 +1,26 @@
+from subclass_attendee_report import *
+from class_flights import *
+from subclass_flight_report import *
+from subclass_staff import *
+from sub_class_passengers import *
+
+# Set up
+pioneer = AttendeeReport('Pioneer')
+
+
+# Explain
+print("Test 1:")         # Append items to report list
+print("Testing: append_manifest()...")
+# Test
+pioneer.append_manifest(passenger1)
+pioneer.append_manifest(passenger2)
+
+if passenger1 in pioneer.manifest:
+    print("Append complete: ", True)
+else:
+    print("Append complete: ", False)
+# Explain
+print("\nTest 2:")         # Produce full report
+print("Testing: print_manifest()...")
+# Test
+pioneer.print_manifest()
