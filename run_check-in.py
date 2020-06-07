72a5cb2da244520fe72c9264875bf4fb09f05629
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-07 19:03:13 +0100

    Minor changes

--- run_check-in.py
+++ run_check-in.py
@@ -1,10 +1,8 @@
-from class_people import *
 from subclass_passengers import *
-from subclass_staff import *
 from class_flights import *
-from subclass_flight_report import *
 
-pioneer = FlightReport('Pioneer', '0800', 'Paris', 290, 'A321', 300)
+
+pioneer = Flight('Pioneer', '0800', 'Paris', 290, 'A321', 300)
 
 password = 'Happy flighting'
 entry = ''
@@ -20,16 +18,26 @@ while entry != password:
     else:
         print("Incorrect password")
 
-
-print('Current flights in progress: ')
+print('Current flight in progress: ')
 print(Flight.instances)
 
-
 while objective != "Exit":
-    objective = input("\nWhat service would you like to access? \n Flight Status \n Flight Report \n Assign Staff \n Add Passenger \n Exit     \n                                 ")
+    objective = input(
+        "\nWhat service would you like to access? \n Flight Status \n Flight Report \n Assign Staff \n Add Passenger \n Exit     \n                                 ")
 
-    if objective == "New Passenger":
-        break
+    if objective == "Add Passenger":
+        flight = input("Enter desired flight:   ")
+        new_name = input("Enter passenger name:   ")
+        new_dob = input("Enter passenger dob:   ")
+        new_nationality = input("Enter passenger nationality:   ")
+        new_pp = input("Enter passenger passport details:   ")
+        new_t_type = input("Requested ticket type:   ")
+        new_bp_raw = "BA-", str(randint(999, 9999)), "-", str(randint(999, 9999))
+        new_bp = ''.join(new_bp_raw)
+
+        passenger4 = Passenger(new_name, new_dob, new_nationality, new_pp, new_t_type, new_bp)
+        passenger4.passenger_details()
+        # Needs appending
     elif objective == "Flight Status":
         break
     elif objective == "Flight Report":
@@ -39,7 +47,7 @@ while objective != "Exit":
     else:
         break
 
-#new passenger
-#flight status
-#flight report
-#assign staff
+# new passenger
+# flight status
+# flight report
+# assign staff
