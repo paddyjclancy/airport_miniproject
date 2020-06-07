3d32532f4eb13cda497e5defd5120e5005121d13
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-07 19:02:25 +0100

    Refactored and simplified

--- class_flights.py
+++ class_flights.py
@@ -1,22 +1,41 @@
 from class_airport import *
 
-# Parent Class = flight()
-#
-# Sub Classes = FlightReport(), AttendeeReport(), Cities(), Planes()
-# ME    # FlightReport() -
-# ME    # AttendeeReport() -
-        # Cities() - destination, distance
-        # Planes() - model, capacity, cruising_speed
-
-# Pseudo
-# TDD
-# Code
-
 
 class Flight(Airport):
     instances = []
 
-    def __init__(self, flight, departure):
+    def __init__(self, flight, departure, destination, flight_time, plane, capacity, manifest=None):
         self.flight = flight.capitalize()
-        self.departure = departure
+        self.departure = int(departure)
         Flight.instances.append(self.flight)
+        self.destination = destination.capitalize()
+        self.flight_time = flight_time
+        self.plane = plane.capitalize()
+        self.capacity = capacity
+        self.manifest = manifest
+        if manifest is None:
+            self.manifest = []
+
+    def delay(self, new_time):
+        self.departure = new_time
+
+    def divert(self, new_destination):
+        self.destination = new_destination
+
+    def alter_aircraft(self, new_plane):
+        self.plane = new_plane
+
+    def show_report(self):
+        report = vars(self)
+        for key, value in report.items():
+            print(key.capitalize(), ": ", value)
+
+    def append_manifest(self, passenger):
+        self.manifest.append(passenger)
+
+    def show_manifest(self):
+        print(f"\nManifest for personnel on flight {self.flight}: ")
+        for person in self.manifest:
+            print(vars(person))
+
+
--- class_people.py
+++ class_people.py
@@ -12,20 +12,19 @@ class People(Airport):
         return self.__name
 
     def set_full_name(self, new_name):
-        print(f"Changing name for person:  {self.__name}")
         self.__name = new_name
-        print(f"Name overridden to:  {self.__name}")
+        return f"Name overridden to:  {self.__name}"
 
     def get_dob(self):
         return self.__dob
 
     def set_dob(self, new_dob):
         self.__dob = new_dob
-        print(f"DOB for {self.__name} overridden to:  {self.__dob}")
+        return "DOB for {self.__name} overridden to:  {self.__dob}"
 
     def get_nationality(self):
         return self.nationality
 
     def set_nationality(self, new_nation):
         self.nationality = new_nation
-        print(f"Nationality for {self.__name} overridden to:  {self.nationality}")
+        return "Nationality for {self.__name} overridden to:  {self.nationality}"
